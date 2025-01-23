f8f401b2434de8c00002fa502eebf4697c28aab2
ukilolll <ukikillme@gmail.com>
2025-01-23 14:57:40 +0700

    save file in json

--- index.py
+++ index.py
@@ -1,4 +1,8 @@
 import requests
+import os
 
 res = requests.get("https://minecraft.fandom.com/wiki/Crafting")
-data = res.content
+data = res.json()
+f = open("db/user.json","w")
+f.write(data)
+f.close()
