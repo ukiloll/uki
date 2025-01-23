b806e82916bb25e689df5482097bcb79657c9346
ukilolll <ukikillme@gmail.com>
2025-01-23 16:31:31 +0700

    read json file

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 import requests
-import os
+import json
 
 res = requests.get("https://minecraft.fandom.com/wiki/Crafting")
 data = res.json()
 f = open("db/user.json","w")
 f.write(data)
 f.close()
+
+f = open("db/user.json","r")
+obj = json.load(f.readlines())
